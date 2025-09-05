ecf2c9b7ba5abaf943e7fadfcda341c8ba7480bd
ICE MrThink15000 <a01287002@tec.mx>
2025-09-05 15:09:20 -0600

    First commit

---
+++ src/main.py
@@ -0,0 +1,13 @@
+'''
+#       Sesion 4: For and while loops
+#       Andrés Rodríguez Cantú & Ethiel Favila Alvarado  ─ A01287002
+#
+#       Copyright (C) Tecnológico de Monterrey
+#
+#       File: src/main.py
+#
+#       Created:                09/05/2025
+#       Last Modified:          09/05/2025
+'''
+
+print("Hello World")
